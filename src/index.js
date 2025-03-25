ddd75282b756e6a75d881e1ab78ca6658c97c7e9
beerezina <berezinapk@yandex.ru>
2025-03-25 22:44:36 +0300

    try

--- src/index.js
+++ src/index.js
@@ -123,3 +123,59 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 })
+
+// фильтрация
+
+// function toggleMainFilter(category, element) {
+//   document
+//     .querySelectorAll('.A_FilterBtn')
+//     .forEach((btn) => btn.classList.remove('active'))
+//   element.classList.add('active')
+
+//   document.getElementById('M_WordSubfilters').style.display =
+//     category === 'words' ? 'flex' : 'none'
+//   document.getElementById('M_PhraseSubfilters').style.display =
+//     category === 'phrases' ? 'flex' : 'none'
+
+//   filterCards(category)
+// }
+
+// function filterCards(category, element = null) {
+//   const cards = document.querySelectorAll('.O_Card')
+//   cards.forEach((card) => {
+//     const matchWords = [
+//       'how-how',
+//       'who-who',
+//       'things',
+//       'whatdoing',
+//       'tenderness',
+//     ]
+//     const matchPhrases = ['proverbs', 'sayings', 'idioms']
+
+//     if (
+//       category === 'all' ||
+//       card.classList.contains(category) ||
+//       (category === 'words' &&
+//         matchWords.some((c) => card.classList.contains(c))) ||
+//       (category === 'phrases' &&
+//         matchPhrases.some((c) => card.classList.contains(c)))
+//     ) {
+//       card.style.display = 'block'
+//     } else {
+//       card.style.display = 'none'
+//     }
+//   })
+
+//   if (element) {
+//     document
+//       .querySelectorAll('.A_SubFilterBtn')
+//       .forEach((btn) => btn.classList.remove('active'))
+//     element.classList.add('active')
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   toggleMainFilter('all', document.querySelector('.A_FilterBtn.active'))
+// })
+// window.toggleMainFilter = toggleMainFilter
+// window.filterCards = filterCards
